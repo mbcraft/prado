f6b22febb41b3f552e36d5d0190ce8672b4d6d6e
Fabio Bas <ctrlaltca@gmail.com>
2015-01-20 22:54:21 +0100

    one class per file: framework/Web/UI/*.php

---
+++ framework/Web/UI/ISurroundable.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * TControl, TControlCollection, TEventParameter and INamingContainer class file
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @link http://www.pradosoft.com/
+ * @copyright Copyright &copy; 2005-2014 PradoSoft
+ * @license http://www.pradosoft.com/license/
+ * @package System.Web.UI
+ */
+
+/**
+ * ISurroundable interface
+ *
+ * Identifies controls that may create an additional surrounding tag. The id of the
+ * tag can be obtained with {@link getSurroundingTagID}.
+ *
+ * @package System.Web.UI
+ * @since 3.1.2
+ */
+interface ISurroundable
+{
+	/**
+	 * @return string the id of the embedding tag of the control or the control's clientID if not surrounded
+	 */
+	public function getSurroundingTagID();
+}
